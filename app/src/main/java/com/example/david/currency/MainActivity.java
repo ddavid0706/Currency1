464c5b5861cc67901298dc24d4c6a64724f71ccb
董家成 <ddavid0706tw@gmail.com>
2018-10-18 11:14:04 +0800

    增加多國語系(TW)、TextView 顯示日幣與美金換算

--- app/src/main/java/com/example/david/currency/MainActivity.java
+++ app/src/main/java/com/example/david/currency/MainActivity.java
@@ -6,10 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText ndt;
+    private TextView tv_jp,tv_usd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
 
     private void findView() {
         ndt = (EditText)findViewById(R.id.ed_NTD);
+        tv_jp = (TextView)findViewById(R.id.tv_jp);
+        tv_usd= (TextView)findViewById(R.id.tv_usd);
     }
 
     public void ndttousa(View view) {
@@ -28,11 +32,13 @@ public class MainActivity extends AppCompatActivity {
         if (str.length() != 0 ){
             double NDT = Double.parseDouble(str);
             float usd = 30.9f;
-            double result = (NDT/usd);
+            float jp = 3.64f;
+            double usd_result = (NDT/usd);
+            double jp_result = NDT * jp;
             new AlertDialog.Builder(this)
-                    .setTitle("Result")
-                    .setMessage("USD is " + (float)result)
-                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    .setTitle(R.string.result)
+                    .setMessage(getString(R.string.usd_is) + (float)usd_result)
+                    .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
                             ndt.setText("");
@@ -40,12 +46,18 @@ public class MainActivity extends AppCompatActivity {
                     })
                     .show();
 
+                tv_usd.setText((float)usd_result + "$");
+                tv_jp.setText((float)jp_result + "¥");
+
+
         }else{
             new AlertDialog.Builder(this)
-                    .setTitle("Problem")
-                    .setMessage("Please enter you NTD amount")
-                    .setPositiveButton("OK", null )
+                    .setTitle(R.string.problem)
+                    .setMessage(R.string.please_enter_ntd)
+                    .setPositiveButton(R.string.ok, null )
                     .show();
+            tv_usd.setText("無");
+            tv_jp.setText("無");
         }
 
     }
